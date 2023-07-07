5768acfd96fc261a60552f818d0a15465ac1900f
Cbmariserla2 <138811750+Cbmariserla2@users.noreply.github.com>
2023-07-07 22:00:33 +0530

    Update script.js

--- week1/script.js
+++ week1/script.js
@@ -1,4 +1,4 @@
-var button = document.getElementById("demobutton");
+var button = document.getElementById("my-button");
 
 button.addEventListener("click", function() {
     alert("Thanks! You clicked the button! :)");
